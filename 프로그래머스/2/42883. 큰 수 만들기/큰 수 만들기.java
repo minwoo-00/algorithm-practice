fde45a18cd15cbd6d7f26787d7b9bccda32862b8
MinWoo Cho <um9375@naver.com>
2026-02-17 16:55:11 +0900

    [level 2] Title: 큰 수 만들기, Time: 6704.43 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,9 +1,9 @@
 class Solution {
     public String solution(String number, int k) {
-        int select = number.length() - k;
+        int select = number.length() - k; //남아있어야 할 숫자의 수
         char[] answer = new char[select];
         
-        int minIdx = 0;
+        int minIdx = 0; 
         int maxIdx = k;
         
         char max = '0' - 1;
@@ -12,7 +12,7 @@ class Solution {
         
         while(select > 0){
             
-            for(int i = minIdx; i <= maxIdx; i++){
+            for(int i = minIdx; i <= maxIdx; i++){ //범위 내에서 가장 큰 수 구하기
                 if(number.charAt(i) > max){
                     max = number.charAt(i);
                     curIdx = i;
@@ -20,9 +20,10 @@ class Solution {
             }
             
             answer[ansIdx] = number.charAt(curIdx);
-            select--;
             ansIdx++;
-            minIdx = curIdx+1;
+            select--;
+            
+            minIdx = curIdx+1; //선택한 curIdx 다음부터 다시 탐색하도록 minIdx 설정
             maxIdx++;
             max = '0' - 1;
             
