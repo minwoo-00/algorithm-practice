cc3b33661f63b89fb7ab6d989cc9c12ef62f9b54
MinWoo Cho <um9375@naver.com>
2026-02-22 20:25:17 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, to_char(date_of_birth, 'yyyy-mm-dd') as date_of_birth from member_profile where to_char(date_of_birth, 'MM') = '03' and gender = 'W' and tlno is not null order by member_id;
