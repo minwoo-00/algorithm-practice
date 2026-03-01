5d2ef1eb339e9bca581883f80abe50853f9a7ad5
MinWoo Cho <um9375@naver.com>
2026-03-01 22:44:38 +0900

    [level 2] Title: H-Index, Time: 0.93 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int n = citations.length;
+        Arrays.sort(citations);
+
+        for(int i = n-1; i>=0; i--){
+            if(citations[i] >= n-i){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
